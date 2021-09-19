98ab546ef87dab14c204bdce61df2c4f3d6030b9
Prathik B Shetty <prathikshetty9b@gmail.com>
2021-09-19 12:52:18 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,66 @@
+
+// Button Click 
+for(var i = 0 ; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        drumSound(this.className[0]);
+        buttonAnimation(this.className[0]);
+    });
+    
+}
+
+// keyboard input
+document.addEventListener("keypress",function(event){
+    drumSound(event.key);
+    buttonAnimation(event.key);
+})
+
+// Function to play drum sounds
+function drumSound(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "k":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        case "l":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+    
+        default:
+            break;
+    }
+}
+
+// Button Animation
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");}, 100);
+    }
